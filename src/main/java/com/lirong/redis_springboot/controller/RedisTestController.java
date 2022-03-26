30d27d0a651f23bdace7dce7d8cb3e8e91f3bb16
lirong <1437720670@qq.com>
2022-03-26 11:51:24 +0800

    second commit

---
+++ src/main/java/com/lirong/redis_springboot/controller/RedisTestController.java
@@ -0,0 +1,25 @@
+package com.lirong.redis_springboot.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/redisTest")
+public class RedisTestController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    // 测试提交；
+    @GetMapping
+    public String testRedis(){
+        redisTemplate.opsForValue().set("name","lucy");
+        String name = (String) redisTemplate.opsForValue().get("name");
+        System.out.println("laldas");
+        return name;
+    }
+}
